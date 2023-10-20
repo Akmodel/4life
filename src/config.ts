51d09d91c08e0bbf7892dbe58621fa5503dc6aff
DerrickNgigi <ngigid0@gmail.com>
2023-10-20 04:06:53 +0300

    push cyclic version

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const SERVER_PORT = process.env.SERVER_PORT || 5000;
+export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'http://ec2-54-173-232-147.compute-1.amazonaws.com';
+export const OPENVIDU_SECRET = process.env.OPENVIDU_SECRET || 'MY_SECRET';
+export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE || 'selfsigned';
+export const CALL_PRIVATE_ACCESS = process.env.CALL_PRIVATE_ACCESS || 'ENABLED';
+export const CALL_USER = process.env.CALL_USER || 'admin';
+export const CALL_SECRET = process.env.CALL_SECRET || OPENVIDU_SECRET;
+export const CALL_ADMIN_SECRET = process.env.CALL_ADMIN_SECRET || OPENVIDU_SECRET;
+export const CALL_RECORDING = process.env.CALL_RECORDING || 'ENABLED';
+export const CALL_BROADCAST = process.env.CALL_BROADCAST || 'ENABLED';
